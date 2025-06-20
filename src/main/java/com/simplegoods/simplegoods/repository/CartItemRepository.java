33a5f69d1bd7245c3b4215b849f5943b18dc7565
Tharusha Peiris <101708782+peiris830@users.noreply.github.com>
2025-06-20 15:32:36 +0530

    add JPA repository interfaces for User, Product and CartItem

---
+++ src/main/java/com/simplegoods/simplegoods/repository/CartItemRepository.java
@@ -0,0 +1,13 @@
+package com.simplegoods.simplegoods.repository;
+
+import com.simplegoods.simplegoods.model.CartItem;
+import com.simplegoods.simplegoods.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByUser(User user);
+
+    void deleteByUserAndProductId(User user, Long productId);
+}
